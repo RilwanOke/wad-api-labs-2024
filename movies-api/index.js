d6191cfc9150d5297bd9d83de8f23c88e276b678
Rilwan <20102311@wit.ie>
2024-12-22 05:24:31 +0000

    Added Movies Route

--- movies-api/index.js
+++ movies-api/index.js
@@ -3,7 +3,8 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import usersRouter from './api/users';
 import './db';
-import defaultErrHandler from './errHandler'
+import defaultErrHandler from './errHandler';
+import moviesRouter from './api/movies';   //import movies router
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/movies', moviesRouter); //ADD THIS BEFORE THE DEFAULT ERROR HANDLER.
 app.use('/api/users', usersRouter);
 app.use(defaultErrHandler);
 
