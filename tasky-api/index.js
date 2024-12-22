4a5b554ea300dae64318fc78d94cd5d9426a677d
Rilwan <20102311@wit.ie>
2024-12-22 03:10:18 +0000

    Mongo lab Users Router

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -2,6 +2,9 @@ import './db';
 import dotenv from 'dotenv';
 import express from 'express';
 import tasksRouter from './api/tasks';
+//... other imports
+import usersRouter from './api/users';
+
 
 dotenv.config();
 
@@ -22,6 +25,11 @@ const port = process.env.PORT;
 app.use(express.json());
 
 app.use('/api/tasks', tasksRouter);
+
+
+
+//Users router
+app.use('/api/users', usersRouter);
 
 app.use(errHandler);
 
