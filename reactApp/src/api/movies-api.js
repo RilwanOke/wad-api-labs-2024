acb59d22b6ac396eee65e16f6a4ac2e7d3eabdee
Rilwan <20102311@wit.ie>
2024-12-22 06:46:21 +0000

    Integration Lab - Get Movies from API

--- reactApp/src/api/movies-api.js
+++ reactApp/src/api/movies-api.js
@@ -1,9 +1,13 @@
 export const getMovies = async () => {
-    const response = await  fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=34d827c8cbac0a8530dccb55b43f7d4c&language=en-US&include_adult=false&page=1`
-    )
-    return response.json()
-  };
+  const response = await fetch(
+    'http://localhost:8080/api/movies', {
+    headers: {
+      'Authorization': window.localStorage.getItem('token')
+    }
+  }
+  )
+  return response.json();
+};
   export const login = async (username, password) => {
     const response = await fetch('http://localhost:8080/api/users', {
         headers: {
